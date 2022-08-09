809999da5e594e03d44f0a8e491055e17315624e
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-08-09 17:49:30 +0200

    add main.rb file, create basic UI

---
+++ classes/list.rb
@@ -0,0 +1,44 @@
+class List
+  def initialize
+    @books = []
+    @music_albums = []
+    @movies = []
+    @games = []
+    @genres = []
+    @labels = []
+    @authors = []
+    @sources = []
+  end
+
+  def list_all_books
+    puts 'There is no book!'
+  end
+
+  def list_all_music_albums
+    puts 'There is no music_album!'
+  end
+
+  def list_all_movies
+    puts 'There is no movies!'
+  end
+
+  def list_all_games
+    puts 'There is no games!'
+  end
+
+  def list_all_genres
+    puts 'There is no genres!'
+  end
+
+  def list_all_labels
+    puts 'There is no labels!'
+  end
+
+  def list_all_authors
+    puts 'There is no authors!'
+  end
+
+  def list_all_sources
+    puts 'There is no sources!'
+  end
+end
