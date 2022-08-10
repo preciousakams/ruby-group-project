8845758ebc1ce7884efea7cfb828b1fcf34f2a8a
Precious Akams <preciousakams@yahoo.com>
2022-08-10 22:53:11 +0100

    book details

--- classes/book.rb
+++ classes/book.rb
@@ -1,12 +1,13 @@
 require '../item'
 class Book
-  def initialize(publisher, cover_state, publish_date = Time.year)
+  def initialize(publisher, cover_state,author, publish_date = Time.year)
     @id = rand(1..100)
     @publisher = publisher
     @cover_state = cover_state
+    @author = author
     @publish_date = publish_date
   end
-  
+
   def can_be_archived?
     true if Time.now.year - @publish_date.year > 10 || @cover_state == 'bad'
   end
