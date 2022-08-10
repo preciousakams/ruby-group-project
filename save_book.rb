b7ef48e30a5858f0194bdea850672369ec5dde1d
Precious Akams <preciousakams@yahoo.com>
2022-08-10 22:54:45 +0100

    save book

---
+++ save_book.rb
@@ -0,0 +1,10 @@
+require 'json'
+require './fetch_book'
+
+module SaveBook
+  def save_book(book)
+    books = fetch_book
+    books.push(publisher: book.publisher, cover_state: book.cover_state, author: book.author, publish_date: book.publish_date)
+    File.write('./all_json_files/books.json', JSON.pretty_generate(books))
+  end
+end
