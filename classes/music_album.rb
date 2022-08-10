31849959ea2ddb84dcf0d1653c770fbb01e1dcff
onomeh <onwuanibeonome@gmail.com>
2022-08-10 12:53:38 +0000

    Create music album class

---
+++ classes/music_album.rb
@@ -0,0 +1,9 @@
+class MusicAlbum
+  attr_reader :album_name, :artist_name, :publish_date, :on_spotify
+  
+  def initialize(album_name, artist_name, publish_date, on_spotify)
+    @album_name = album_name
+    @artist_name = artist_name
+    @on_spotify = on_spotify
+  end
+end
