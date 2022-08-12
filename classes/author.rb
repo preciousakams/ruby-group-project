2f03f82659347cd807798c1f84415e8bb3d11a11
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-08-12 17:50:08 +0200

    create author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,8 +1,16 @@
 class Author
-  def initialize(first_name, last_name)
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
+  def initialize(first_name = 'Unknown', last_name = 'Unknown', id: nil)
     @first_name = first_name
     @last_name = last_name
-    id = Random.rand(1...1000)
+    @id = id || rand(1...1000)
     @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.authors = self
   end
 end
